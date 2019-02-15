2801fb1c656412481c446aab8f09417f774642fc
Ikechi Michael <mykehell123@gmail.com>
2019-02-16 00:50:21 +0100

    feat: spin without css animation

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function createWindow () {
     // Create the browser window.
     let win = new BrowserWindow({ width: 800, height: 600 })
 
+    win.webContents.openDevTools()
+
     // and load the index.html of the app.
     win.loadFile('./src/index.html')
 }
