89444482b57cede0e6b3101f9832379f54729497
Ikechi Michael <mykehell123@gmail.com>
2019-02-16 11:37:16 +0100

    feat: rotate servo at angles

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { app, BrowserWindow } = require('electron')
 
-require('electron-reload')(__dirname);
-
 function createWindow () {
     // Create the browser window.
     let win = new BrowserWindow({ width: 800, height: 600 })
