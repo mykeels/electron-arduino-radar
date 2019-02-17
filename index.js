abeed8909edcd6aa7b38b4b5acf982a2569295f5
Ikechi Michael <mykehell123@gmail.com>
2019-02-17 10:13:17 +0100

    feat: turn the servo incrementally, and read input from the sonar

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
     // Create the browser window.
-    let win = new BrowserWindow({ width: 800, height: 600 })
+    let win = new BrowserWindow({ width: 600, height: 600 })
 
-    win.webContents.openDevTools()
+    if (process.env.DEV) win.webContents.openDevTools()
 
     // and load the index.html of the app.
     win.loadFile('./src/index.html')
